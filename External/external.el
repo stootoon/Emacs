88e7c43e18572a062b564506d7a0e493aa3577f4
stootoon <sina.tootoonian@gmail.com>
2015-10-30 15:14:36 +0000

    Script containing misc code from external sources to be loaded from dot-emacs.el

---
+++ External/external.el
@@ -0,0 +1,13 @@
+;; Code from external sources to be loaded from dot-emacs.
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (interactive)
+  (set-window-margins win
+          (ceiling (* (if (boundp 'text-scale-mode-step)
+                  (expt text-scale-mode-step
+                    text-scale-mode-amount) 1)
+              (if (car (window-margins))
+                  (car (window-margins)) 1)
+              ))))
+
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
