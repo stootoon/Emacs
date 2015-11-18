d49f53938d436387328d754b60ce250cf14417da
stootoon <sina.tootoonian@gmail.com>
2015-11-18 15:51:24 +0000

    addeded keybindings for window-numbering-mode.

--- dot-emacs.el
+++ dot-emacs.el
@@ -537,4 +537,14 @@
 (global-set-key (kbd "C-; C-g c") '(lambda () (interactive) (insert "\\xi"))) 
 (global-set-key (kbd "C-; C-g w") '(lambda () (interactive) (insert "\\psi"))) 
 (global-set-key (kbd "C-; C-g o") '(lambda () (interactive) (insert "\\omega"))) 
-
+;;; Window numbering
+(global-set-key (kbd "s-0") 'select-window-0)
+(global-set-key (kbd "s-1") 'select-window-1)
+(global-set-key (kbd "s-2") 'select-window-2)
+(global-set-key (kbd "s-3") 'select-window-3)
+(global-set-key (kbd "s-4") 'select-window-4)
+(global-set-key (kbd "s-5") 'select-window-5)
+(global-set-key (kbd "s-6") 'select-window-6)
+(global-set-key (kbd "s-7") 'select-window-7)
+(global-set-key (kbd "s-8") 'select-window-8)
+(global-set-key (kbd "s-9") 'select-window-9)
